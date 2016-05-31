bac26d0efc667933e39bde9723077d7cd7777e88
Austin Purcell <austin.purcell1@gmail.com>
2016-05-30 17:35:51 -0700

    Trying to remove redundant file.

--- MinimumShootEmUp/src/minshmup/gameinterface/observer/IObserver.java
+++
@@ -1,6 +0,0 @@
-package minshmup.gameinterface.observer;
-
-public interface IObserver 
-{
-	public void update(IObservable obs, Object arg0);
-}
--- MinimumShootEmUp/src/minshmup/gameinterface/strategy/IMovementStrategy.java
+++
@@ -1,7 +0,0 @@
-package minshmup.gameinterface.strategy;
-
-public interface IMovementStrategy 
-{//movement strategies for weapons and ships
-	
-	public void apply();//does the thing
-}
